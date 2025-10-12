85c1ba2ded7e16d2e348b9e6a6bf22ec61594f72
jhk <qwertyjhk11@naver.com>
2025-10-12 13:25:20 -0700

    feat: TypeORM 및 AuthModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileModule } from './file/file.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    FileModule,
+    TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_DATABASE || 'web_3d_viewer',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: process.env.NODE_ENV !== 'production', // 개발환경에서만 true
+        logging: process.env.NODE_ENV === 'development', // 개발환경에서 SQL 로그
+      }),
+      AuthModule,
+      FileModule,
   ],
 })
 export class AppModule {}
