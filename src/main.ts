4e8889179b6afdcae053a8734c24aeb8de4cafbd
jhk <qwertyjhk11@naver.com>
2025-10-15 17:27:08 +0900

    feat: CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8080', // 요청을 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 허용할 HTTP 메서드
+    allowedHeaders: 'Content-Type, Accept, Authorization', // 허용할 HTTP 헤더
+    credentials: true, // 자격 증명(쿠키 등)을 포함한 요청 허용
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
