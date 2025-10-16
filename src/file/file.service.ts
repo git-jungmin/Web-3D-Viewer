2677b3de0d190e376d723411782442aeb75a5011
jhk <qwertyjhk11@naver.com>
2025-10-16 09:59:54 +0900

    feat: 메타데이터 포함 파일 업로드 기능 개선

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,26 +3,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { FileEntity } from './file.entity';
 import { Repository } from 'typeorm';
 import { S3Service } from './s3.service';
+import { UploadFileDto, UploadFileResponseDto } from './dto/upload-file.dto';
 @Injectable()
 export class FileService {
     constructor(
         @InjectRepository(FileEntity)
-        private readonly fileRepo: Repository<FileEntity>,
+        private readonly fileRepository: Repository<FileEntity>,
         private readonly s3Service: S3Service,
     ) {}
 
     async upload(
         file: Express.Multer.File,
-    ): Promise<{ key: string; url: string }> {
+        uploadFileDto: UploadFileDto
+    ): Promise<UploadFileResponseDto> {
         const s3Result = await this.s3Service.uploadFile(file);
-        const saved = this.fileRepo.create({
+        const saved = this.fileRepository.create({
             originalName: file.originalname,
             s3Key: s3Result.Key,
             s3Url: s3Result.Location,
             fileType: file.mimetype,
+            title: uploadFileDto.title,
+            description: uploadFileDto.description,
+            tags: uploadFileDto.tags
         });
 
-        await this.fileRepo.save(saved);
+        await this.fileRepository.save(saved);
 
         return {
             key: s3Result.Key,
@@ -32,6 +37,6 @@ export class FileService {
 
     async deleteFile(s3Key: string): Promise<void> {
         await this.s3Service.deleteFile(s3Key);
-        await this.fileRepo.delete({ s3Key });
+        await this.fileRepository.delete({ s3Key });
     }
 }
